e46f12cf2c49e9803665e498679dea6c8b7d3edf
krobinson-netint <123592491+krobinson-netint@users.noreply.github.com>
2023-04-25 14:09:03 -0400

    updated to save as mp4

--- roi.sh
+++ roi.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ffmpeg -y -dec 0 -c:v h264_ni_quadra_dec -xcoder-params 'out=hw' -i source/roi_1080p-source.h264 -vf 'ni_quadra_roi=nb=./source/network_binary_yolov4_head.nb:qpoffset=-0.6' -enc 0 -c:v h264_ni_quadra_enc -xcoder-params 'roiEnable=1:RcEnable=1:bitrate=500000' -an roi_1080p-output.h264
+ffmpeg -y -dec 0 -c:v h264_ni_quadra_dec -xcoder-params 'out=hw' -i source/roi_1080p-source.h264 -vf 'ni_quadra_roi=nb=./source/network_binary_yolov4_head.nb:qpoffset=-0.6' -enc 0 -c:v h264_ni_quadra_enc -xcoder-params 'roiEnable=1:RcEnable=1:bitrate=500000' -an roi_1080p-output.mp4
