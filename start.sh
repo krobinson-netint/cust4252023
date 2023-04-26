563a6f4f66af6af8adcc3cc216d53ae34357579c
krobinson-netint <kenneth.robinson@netint.ca>
2023-04-26 09:13:47 -0700

    updated start.sh

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ sudo mkdir /tmp/ramdisk
 sudo chmod 777 /tmp/ramdisk
 sudo mount -t tmpfs -o size=150G myramdisk /tmp/ramdisk
 
-cp ~/twitter/source/*.mp4 /tmp/ramdisk
+cp ~/testscripts/source/*.mp4 /tmp/ramdisk
