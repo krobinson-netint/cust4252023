86d8f34417f5a261619dffb62b3d777eeeb39ef6
krobinson-netint <kenneth.robinson@netint.ca>
2023-04-27 06:29:59 -0700

    added another directory to for ramdisk

--- create-ramdisk.sh
+++ create-ramdisk.sh
@@ -5,4 +5,5 @@ sudo mkdir /tmp/ramdisk
 sudo chmod 777 /tmp/ramdisk
 sudo mount -t tmpfs -o size=150G myramdisk /tmp/ramdisk
 
-cp ~/testscripts/source/*.mp4 /tmp/ramdisk
+cp ~/testscripts/source/* /tmp/ramdisk
+cp ~/video-source/* /tmp/ramdisk
